398b875a2f63a3ef7005cf15f24cb289956b6054
Elvin <ttop34@icloud.com>
2017-09-07 09:41:06 -0500

    created our ajax request *displayed tempeture to div in html

--- js/weather-app.js
+++ js/weather-app.js
@@ -1,8 +1,20 @@
 "use strict";
 (function () {
 
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather",
+    type: "GET",
+    data: {
+        APPID: "34b135188b7c0ee901808521cc501b28",
+        q:  "San Antonio, TX",
+        units: "imperial"
+    }
 
+}).done(function(data){
+    $('#weather').html(data.main.temp);
+console.log(data);
 
+    });
 
 
 
