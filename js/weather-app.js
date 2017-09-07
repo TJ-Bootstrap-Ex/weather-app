570a8fc91e98a82db2e40a9b7dc4beec1a486cbf
Elvin <ttop34@icloud.com>
2017-09-07 10:53:40 -0500

    add google maps API *add weather map css

--- js/weather-app.js
+++ js/weather-app.js
@@ -20,7 +20,7 @@ $.ajax({
     let pressure = data.main.pressure;
     let des = data.weather[0].description;
     let iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-    //
+
     $('#weather').html(minTemp + "&#176" + " / " + maxTemp + "&#176" + "<br>");
     $("#weather").append("<img src='" + iconUrl  + "'>" + "<br>");
     $('#weather').append(main + ": " + des + "<br>");
@@ -29,9 +29,22 @@ $.ajax({
     $('#weather').append("Pressure: " + pressure + "<br>");
     console.log(data);
 
-// console.log(data);
-
     });
+
+    // Set our map options
+    var mapOptions = {
+        // Set the zoom level
+        zoom: 16,
+
+        // This sets the center of the map at our location
+        center: {
+            lat:  29.426791,
+            lng: -98.489602
+        }
+    };
+
+    // Render the map
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 
 
