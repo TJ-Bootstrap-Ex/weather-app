8d267856ac3a51a38a4b63cb5535f4fdddef7c04
Elvin <ttop34@icloud.com>
2017-09-07 10:33:56 -0500

    complete all data for display

--- js/weather-app.js
+++ js/weather-app.js
@@ -15,13 +15,18 @@ $.ajax({
     let minTemp = data.main.temp_min;
     let icon = data.weather[0].icon;
     let main = data.weather[0].main;
+    let wind = data.wind.speed;
+    let humiidity = data.main.humidity;
+    let pressure = data.main.pressure;
     let des = data.weather[0].description;
     let iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
     //
     $('#weather').html(minTemp + "&#176" + " / " + maxTemp + "&#176" + "<br>");
     $("#weather").append("<img src='" + iconUrl  + "'>" + "<br>");
     $('#weather').append(main + ": " + des + "<br>");
-
+    $('#weather').append("Humidity: " + humiidity + "<br>");
+    $('#weather').append("Wind: " + wind + "<br>");
+    $('#weather').append("Pressure: " + pressure + "<br>");
     console.log(data);
 
 // console.log(data);
